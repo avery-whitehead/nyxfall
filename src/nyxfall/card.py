b7a8bc69f2774f78b776e616bc466aa875e81c9c
Avery Abrahams-Whitehead <averywhthead@gmail.com>
2025-02-27 23:57:01 -0800

    Use Unicode characters to draw box by default

--- src/nyxfall/card.py
+++ src/nyxfall/card.py
@@ -47,27 +47,27 @@ class Card:
         # of the display. It will be appended to later and joined with a newline separator at the end
         card = [
             # Name and mana cost
-            f"+{"-" * card_width}+",
-            f"|{self.name}{" " * (card_width - len(self.name) - len(self.mana_cost))}{self.mana_cost}|",
-            f"+{"-" * card_width}+",
+            f"┌{"─" * card_width}┐",
+            f"│{self.name}{" " * (card_width - len(self.name) - len(self.mana_cost))}{self.mana_cost}│",
+            f"├{"─" * card_width}┤",
             # (empty) image box
-            "\n".join([f"|{" " * card_width}|"] * 3),
-            f"+{"-" * card_width}+",
+            # "\n".join([f"│{" " * card_width}│"] * 3),
+            # f"├{"─" * card_width}┤",
         ]
 
         # Type line
         if len(self.type_line) < card_width:
-            card.append(f"|{self.type_line}{" " * (card_width - len(self.type_line))}|")
+            card.append(f"│{self.type_line}{" " * (card_width - len(self.type_line))}│")
         else:
             card.append(self._wrap_and_pad(self.type_line, card_width))
-        card.append(f"+{"-" * card_width}+")
+        card.append(f"├{"─" * card_width}┤")
 
         # Oracle text
         card.append(self._wrap_and_pad(self.oracle_text, card_width))
 
         # Flavour text
         if self.flavor_text:
-            card.append(f"|  {"-" * (card_width - 4)}  |")
+            card.append(f"│  {"─" * (card_width - 4)}  │")
             # Append and prepend flavour text with ANSI escape code for italics
             card.append(
                 "\x1B[3m"
@@ -79,9 +79,9 @@ class Card:
             # Width of characters in power and toughness plus 3 for the forward slash and spacing
             pt_box_width = len(str(self.power)) + len(str(self.toughness)) + 3
             pt_box = [
-                f"|{" " * (card_width - pt_box_width - 1)}+{"-" * pt_box_width}+",
-                f"|{" " * (card_width - pt_box_width - 1)}| {str(self.power)}/{str(self.toughness)} |",
-                f"+{"-" * (card_width - pt_box_width - 1)}+{"-" * pt_box_width}+",
+                f"│{" " * (card_width - pt_box_width - 1)}┌{"─" * pt_box_width}┤",
+                f"│{" " * (card_width - pt_box_width - 1)}│ {str(self.power)}/{str(self.toughness)} │",
+                f"└{"─" * (card_width - pt_box_width - 1)}┴{"─" * pt_box_width}┘",
             ]
             card.extend(pt_box)
         else:
@@ -105,11 +105,11 @@ class Card:
         # sub-lists and then finally return that list joined with newline as a separator
         return "\n".join(
             [
-                f"|{line}{" " * (card_width - len(line))}|"
+                f"│ {line}{" " * (card_width - len(line) - 2)} │"
                 for lines in [
                     fill(
                         paragraph,
-                        width=CARD_DEFAULT_WIDTH,
+                        width=CARD_DEFAULT_WIDTH - 2,
                         replace_whitespace=False,
                     ).split("\n")
                     for paragraph in text.splitlines()
