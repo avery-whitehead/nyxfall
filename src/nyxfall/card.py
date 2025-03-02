6a9e3a17552e951a48ae2ae4e9622ca891da5c82
Avery Abrahams-Whitehead <averywhthead@gmail.com>
2025-03-01 17:18:37 -0800

    Add flag to render card using only basic ASCII

--- src/nyxfall/card.py
+++ src/nyxfall/card.py
@@ -35,8 +35,25 @@ class Card:
     flavor_text: Optional[str]
     set: str
 
-    def format_as_card(self) -> str:
-        """Builds a string that will display a ``Card`` similar to an actual MTG card"""
+    def format_as_card(self, ascii_only: bool = False) -> str:
+        """Builds a string that will display a ``Card`` similar to an actual MTG card
+
+        Args:
+            ascii_only: True if card frame should be rendered using only the basic ASCII set
+
+        Returns:
+            Formatted and newline-separated string that should display a card when printed
+        """
+        down_right = "+" if ascii_only else "┌"
+        down_left = "+" if ascii_only else "┐"
+        up_right = "+" if ascii_only else "└"
+        up_left = "+" if ascii_only else "┘"
+        vertical = "|" if ascii_only else "│"
+        horizontal = "-" if ascii_only else "─"
+        down_horizontal = "+" if ascii_only else "┬"
+        up_horizontal = "+" if ascii_only else "┴"
+        left_vertical = "+" if ascii_only else "┤"
+
         # Defalt card width to 30 characters unless the card has a particularly long name or mana cost
         card_text_width = (
             CARD_TEXT_DEFAULT_WIDTH
@@ -55,46 +72,53 @@ class Card:
         # of the display. It will be appended to later and joined with a newline separator at the end
         card = [
             # Top of outside bounding box
-            f"┌{"─" * (card_text_width + 2)}┐",
+            f"{down_right}{"─" * (card_text_width + 2)}{down_left}",
             # Name and mana cost
-            f"│┌{"─" * card_text_width}┐│",
-            f"││{self.name}{" " * (card_text_width - len(self.name) - len(self.mana_cost))}{self.mana_cost}││",
-            f"│└┬{"─" * (card_text_width - 2)}┬┘│",
+            f"{vertical}{down_right}{horizontal * card_text_width}{down_left}{vertical}",
+            f"{vertical}{vertical}{self.name}{" " * (card_text_width - len(self.name) - len(self.mana_cost))}{self.mana_cost}{vertical}{vertical}",
+            f"{vertical}{up_right}{down_horizontal}{horizontal * (card_text_width - 2)}{down_horizontal}{up_left}{vertical}",
             # Empty image box
-            "\n".join([f"│ │{" " * (card_text_width - 2)}│ │"] * 7),
+            "\n".join(
+                [
+                    f"{vertical} {vertical}{" " * (card_text_width - 2)}{vertical} {vertical}"
+                ]
+                * 1
+            ),
             # Type line
-            f"│┌┴{"─" * (card_text_width - 2)}┴┐│",
-            f"││{self.type_line}{" " * (card_text_width - len(self.type_line))}││",
-            f"│└┬{"─" * (card_text_width - 2)}┬┘│",
+            f"{vertical}{down_right}{up_horizontal}{horizontal * (card_text_width - 2)}{up_horizontal}{down_left}{vertical}",
+            f"{vertical}{vertical}{self.type_line}{" " * (card_text_width - len(self.type_line))}{vertical}{vertical}",
+            f"{vertical}{up_right}{down_horizontal}{horizontal * (card_text_width - 2)}{down_horizontal}{up_left}{vertical}",
             # Oracle text
             self._wrap_and_pad(self.oracle_text, card_text_width),
         ]
 
         # Flavour text
         if self.flavor_text:
-            card.append(f"│ │ {"─" * (card_text_width - 4)} │ │")
+            card.append(
+                f"{vertical} {vertical} {horizontal * (card_text_width - 4)} {vertical} {vertical}"
+            )
             # Append and prepend flavour text with ANSI escape code for italics
             card.append(
-                "\x1B[3m"
+                ("" if ascii_only else "\x1B[3m")
                 + self._wrap_and_pad(self.flavor_text, card_text_width)
-                + "\x1B[23m"
+                + ("" if ascii_only else "\x1B[23m")
             )
 
         if self.power and self.toughness:
             # Width of characters in power and toughness plus 3 for the forward slash and spacing
             pt_box_width = len(str(self.power)) + len(str(self.toughness)) + 3
             pt_box = [
-                f"│ │{" " * (card_text_width - pt_box_width - 4)}┌{"─" * pt_box_width}┐│ │",
-                f"│ └─{"─" * (card_text_width - pt_box_width - 5)}┤ {str(self.power)}/{str(self.toughness)} ├┘ │",
-                f"│ {self.set}{" " * (card_text_width - len(self.set) - pt_box_width - 3)}└─────┘  │",
-                f"└{"─" * (card_text_width + 2)}┘",
+                f"{vertical} {vertical}{" " * (card_text_width - pt_box_width - 4)}{down_right}{horizontal * pt_box_width}{down_left}{vertical} {vertical}",
+                f"{vertical} {up_right}{horizontal * (card_text_width - pt_box_width - 4)}{left_vertical} {str(self.power)}/{str(self.toughness)} {vertical}{up_left} {vertical}",
+                f"{vertical} {self.set}{" " * (card_text_width - len(self.set) - pt_box_width - 3)}{up_right}{horizontal * 5}{up_left}  {vertical}",
+                f"{up_right}{horizontal * (card_text_width + 2)}{up_left}",
             ]
             card.extend(pt_box)
         else:
             set_box = [
-                f"│ └{"─" * (card_text_width - 2)}┘ │",
-                f"│ {self.set}{" " * (card_text_width - len(self.set))} │",
-                f"└{"─" * (card_text_width + 2)}┘",
+                f"{vertical} {up_right}{horizontal * (card_text_width - 2)}{up_left} {vertical}",
+                f"{vertical} {self.set}{" " * (card_text_width - len(self.set))} {vertical}",
+                f"{up_right}{horizontal * (card_text_width + 2)}{up_left}",
             ]
             card.extend(set_box)
 
